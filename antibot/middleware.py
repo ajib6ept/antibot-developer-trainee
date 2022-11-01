4cef3d12790440b328a32eedc7a40f763aadcd07
ajib6ept <asalbert@narod.ru>
2022-11-01 06:56:29 +0300

    define the basic functionality

--- antibot/middleware.py
+++ antibot/middleware.py
@@ -1,4 +1,59 @@
+import ipaddress
+
+from django.conf import settings
+from django.core.cache import cache
+from django.http import HttpResponse
+
+_TIME_SETTINGS = {"s": 1, "m": 60, "h": 60 * 60}
+
+DEFAULT_IP = "127.0.0.1"
+
+
+def get_subnet_from_headers(request, ipv4_mask):
+    req_headers = request.META
+    x_forwarded_for_value = req_headers.get("HTTP_X_FORWARDED_FOR")
+    if x_forwarded_for_value:
+        ip_addr = x_forwarded_for_value.split(",")[-1].strip()
+    else:
+        ip_addr = DEFAULT_IP
+
+    subnet = ipaddress.ip_network(
+        f"{ip_addr}/{ipv4_mask}", strict=False
+    ).network_address
+    return str(subnet)
 
 
 class RatelimitMiddleware:
-    pass
+    def __init__(self, get_response):
+        self.get_response = get_response
+
+    def __call__(self, request):
+
+        try:
+            ipv4_mask = settings.RATELIMIT_IPV4_MASK
+            limit = settings.RATELIMIT_LIMIT
+            block = settings.RATELIMIT_BLOCK
+            pref = settings.RATELIMIT_CACHE_PREFIX
+        except AttributeError:
+            pass
+        try:
+            block_time = int(block[:-1]) * _TIME_SETTINGS[block[-1]]
+            limit_req = int(limit.split("/")[0])
+            limit_ttl = _TIME_SETTINGS[limit.split("/")[1]]
+        except IndexError:
+            pass
+
+        subnet = get_subnet_from_headers(request, ipv4_mask)
+        key = pref + subnet
+
+        if cache.get(f"{key}+ban"):
+            return HttpResponse(status=429)
+
+        added = cache.add(key, 1, timeout=limit_ttl)
+        if not added:
+            cache.incr(key)
+        value = cache.get(key)
+        if value >= limit_req:
+            cache.set(f"{key}+ban", value=1, timeout=block_time)
+
+        return self.get_response(request)
--- antibot/settings.py
+++ antibot/settings.py
@@ -11,7 +11,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 # SECURITY WARNING: keep the secret key used in production secret!
 
-SECRET_KEY = os.environ.get("SECRET_KEY", 'django-insecure-hxt@am-p@20xqic!r8wjo52x8++acfcwpcu4*w59nmg=z(mno9')
+SECRET_KEY = os.environ.get(
+    "SECRET_KEY",
+    "*w59nmg=z(mno9",
+)
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
@@ -22,35 +25,36 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'django.contrib.auth',
-    'django.contrib.contenttypes',
-    'django.contrib.sessions',
-    'django.contrib.messages',
+    "django.contrib.auth",
+    "django.contrib.contenttypes",
+    "django.contrib.sessions",
+    "django.contrib.messages",
 ]
 
 MIDDLEWARE = [
-    'django.middleware.security.SecurityMiddleware',
-    'django.contrib.sessions.middleware.SessionMiddleware',
-    'django.middleware.common.CommonMiddleware',
-    'django.middleware.csrf.CsrfViewMiddleware',
-    'django.contrib.auth.middleware.AuthenticationMiddleware',
-    'django.contrib.messages.middleware.MessageMiddleware',
-    'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    "django.middleware.security.SecurityMiddleware",
+    "django.contrib.sessions.middleware.SessionMiddleware",
+    "django.middleware.common.CommonMiddleware",
+    "django.middleware.csrf.CsrfViewMiddleware",
+    "django.contrib.auth.middleware.AuthenticationMiddleware",
+    "django.contrib.messages.middleware.MessageMiddleware",
+    "django.middleware.clickjacking.XFrameOptionsMiddleware",
+    "antibot.middleware.RatelimitMiddleware",
 ]
 
-ROOT_URLCONF = 'antibot.urls'
+ROOT_URLCONF = "antibot.urls"
 
 
-WSGI_APPLICATION = 'antibot.wsgi.application'
+WSGI_APPLICATION = "antibot.wsgi.application"
 
 
 # Database
 # https://docs.djangoproject.com/en/4.1/ref/settings/#databases
 
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+    "default": {
+        "ENGINE": "django.db.backends.sqlite3",
+        "NAME": BASE_DIR / "db.sqlite3",
     }
 }
 
@@ -58,18 +62,22 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/4.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = "en-us"
 
-TIME_ZONE = 'UTC'
+TIME_ZONE = "UTC"
 
 USE_I18N = True
 
 USE_TZ = True
 
 
-
-
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
 
-DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+
+RATELIMIT_CACHE_PREFIX = "RLCP:"
+RATELIMIT_IPV4_MASK = 24  # подсеть: /24 (маска 255.255.255.0)
+RATELIMIT_LIMIT = "100/m"  # лимит: 100 запросов в минуту
+RATELIMIT_BLOCK = "2m"  # время ожидания после ограничения: 2 минуты
